4270a1661310cf407ccafe11b56020a2085a086b
aschenkuttel <aschenkuttel@gmail.com>
2023-03-16 13:59:34 +0000

    context + basic web3 connection

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,12 +1,63 @@
-import {createContext} from "react"
+import {Component, createContext} from "react"
+import {ethers} from "ethers"
 
-const Context = createContext(null)
+const PeerContext = createContext(null)
 
-export default function ContextProvider({alpha}) {
-    
-    return (
-        <Context.Provider>
+class PeerProvider extends Component {
 
-        </Context.Provider>
-    )
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            address: null
+        }
+
+        this.provider = null
+    }
+
+    connect = async () => {
+        if (this.state.address === null) {
+            await this.provider.send("eth_requestAccounts", [])
+            const signer = this.provider.getSigner()
+            const address = await signer.getAddress()
+
+            this.setState({
+                address: address
+            })
+        }
+    }
+
+    async componentDidMount() {
+        if (window.ethereum) {
+            this.provider = new ethers.BrowserProvider(window.ethereum)
+
+            window.ethereum.on('accountsChanged', async (wallets) => {
+                if (wallets.length === 0) {
+                    this.setState({address: null})
+                } else {
+                    const address = ethers.getAddress(wallets[0])
+                    this.setState({address})
+                }
+            })
+
+            window.ethereum.on('chainChanged', (_) => {
+                window.location.reload()
+            })
+        }
+    }
+
+    render() {
+        return (
+            <PeerContext.Provider value={{
+                address: this.state.address
+            }}>
+                {this.props.children}
+            </PeerContext.Provider>
+        )
+    }
+}
+
+export {
+    PeerProvider,
+    PeerContext
 }
