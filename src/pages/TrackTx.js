483643ce5f3a9a26ae82ac963a0b5a81a89827fd
Fodenga <mail2.so.a@gmail.com>
2023-03-18 01:05:28 +0000

    Changes FE

--- src/pages/TrackTx.js
+++ src/pages/TrackTx.js
@@ -1,10 +1,13 @@
-import {useParams} from 'react-router-dom'
+import {useParams, Link} from 'react-router-dom'
 import {useEffect, useContext, useState} from "react"
 import {PeerContext} from "../components/Context"
 import Page from '../components/Page'
 import Spinner from '../components/Spinner'
 import Invalid from '../components/Invalid'
-import {dateFormat, linkifyTx} from "../utils/parse"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse,faChevronRight, faArrowsRotate, faCheck} from '@fortawesome/free-solid-svg-icons'
+import { faEthereum } from '@fortawesome/free-brands-svg-icons'
+
 
 
 export default function TrackTx() {
@@ -12,6 +15,8 @@ export default function TrackTx() {
     const {transactionID} = useParams()
     const [receivedTx, setReceivedTx] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [isBuyer,setBuyer] = useState(false);
+    const [isSeller,setSeller] = useState(false);
 
     useEffect(() => {
         (async () => {
@@ -20,102 +25,77 @@ export default function TrackTx() {
             if ([tx.seller, tx.buyer].includes(address)) {
                 setReceivedTx(tx)
             }
-
             setLoading(false)
         })()
     }, [])
 
 
-    function getTxState(props) {
-        if (props.seller && props.buyer && !props.completed) {
-            return <span
-                className="inline-flex items-center rounded-full bg-yellow-100 px-3 py-0.5 text-sm font-medium text-yellow-800">
-        On-Going
-        </span>
-        } else if (props.seller && !props.buyer && !props.completed) {
-            return <span
-                className="inline-flex items-center rounded-full bg-blue-100 px-3 py-0.5 text-sm font-medium text-blue-800">
-        Open
-        </span>
-        } else if (props.seller && props.buyer && props.completed) {
-            return <span
-                className="inline-flex items-center rounded-full bg-green-100 px-3 py-0.5 text-sm font-medium text-green-800">
-        Finished
-        </span>
-        } else {
-            return <span
-                className="inline-flex items-center rounded-full bg-gray-100 px-3 py-0.5 text-sm font-medium text-gray-800">
-        N/A
-        </span>
-        }
-    }
+
+    
 
     const content = () => {
         if (loading) {
             return <Spinner/>
-        } else if (receivedTx != null) {
-            return <div className="px-4 sm:px-6 lg:px-8">
+        } else if (receivedTx != null && address == receivedTx.seller) {
+            return <div className="w-full max-w-5xl">
                 <div className="sm:flex sm:items-center">
-                    <div className="sm:flex-auto">
-                        <h1 className="text-base font-semibold leading-6 text-slate-300">
-                            Transaction: {receivedTx.id}
-                        </h1>
+
+                <nav className="flex" aria-label="Breadcrumb">
+                <ol role="list" className="flex items-center space-x-4">
+                    <li>
+                    <div>
+                        <Link to="/" className="text-gray-400 hover:text-gray-500">
+                            <FontAwesomeIcon icon={faHouse}/>
+                        <span className="sr-only">Home</span>
+                        </Link>
                     </div>
-                </div>
-                <div className="mt-8 flow-root">
-                    <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                        <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-                            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
-                                <table className="min-w-full divide-y divide-gray-300">
-                                    <thead className="bg-gray-50">
-                                    <tr>
-                                        <th scope="col"
-                                            className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
-                                            Transaction
-                                        </th>
-                                        <th scope="col"
-                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                            Owner
-                                        </th>
-                                        <th scope="col"
-                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                            Buyer
-                                        </th>
-                                        <th scope="col"
-                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                            State
-                                        </th>
-                                        <th scope="col"
-                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                            Date
-                                        </th>
-                                    </tr>
-                                    </thead>
-                                    <tbody className="divide-y divide-gray-200 bg-white">
+                    </li>
 
-                                    <tr>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                            {linkifyTx(receivedTx.id)}
-                                        </td>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                            {linkifyTx(receivedTx.seller)}
-                                        </td>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                            {receivedTx.buyer ? linkifyTx(receivedTx.buyer) : "No buyer yet"}
-                                        </td>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                            {getTxState(receivedTx)}
-                                        </td>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{dateFormat(receivedTx.timestamp)}</td>
-                                    </tr>
-
-                                    </tbody>
-                                </table>
-                            </div>
+                    <li >
+                        <div className="flex items-center">
+                        <FontAwesomeIcon icon={faChevronRight} className="shrink-0 text-gray-400"/>
+                        <Link
+                            to="/track"
+                            className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                        >
+                            Track
+                        </Link>
                         </div>
-                    </div>
+                    </li>
+
+                    <li>
+                        <div className="flex items-center">
+                        <FontAwesomeIcon icon={faChevronRight} className="shrink-0 text-gray-400"/>
+                        <Link
+                            to='/' disabled={true}
+                            className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                        >
+                            {receivedTx.id}
+                        </Link>
+                        </div>
+                    </li>
+                </ol>
+                </nav>
+                
+                    
+                
                 </div>
+
+                <div className="h-full bg-gray-800 border border-gray-700 rounded-xl px-8 py-4">
+                    <div className="flex h-24 justify-between">
+                    <FontAwesomeIcon icon={faEthereum} className="text-8xl text-gray-400 shrink-0"/>
+                    <div className="flex-1 h-1/2 border-b-2 border-gray-400 mx-8"/>
+                    <FontAwesomeIcon icon={faArrowsRotate} className="text-8xl text-gray-400 shrink-0"/>
+                    <div className="flex-1 h-1/2 border-b-2 border-gray-400 mx-8"/>
+                    <FontAwesomeIcon icon={faCheck} className="text-8xl text-gray-400 shrink-0"/>
+                </div>
+                
+                    
+                </div>
+                
+                
             </div>
+            
 
         } else {
             return <Invalid/>
